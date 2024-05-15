bddbb00e13d75f2dd903479cc72795153ac4739a
wk <wiktorkx5@gmail.com>
2024-05-15 18:07:41 +0200

    feat: added features printf, add, mul, toint, toreal

--- LLVMActions.java
+++ LLVMActions.java
@@ -1,10 +1,24 @@
-
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Stack;
+
+enum VarType{ INT, REAL, UNKNOWN}
+
+class Value{
+         public String name;
+         public VarType type;
+         public Value(String name, VarType type){
+            this.name = name;
+            this.type = type;
+         }
+}
 
 
 public class LLVMActions extends gramBaseListener {
 
-    HashSet<String> variables = new HashSet<String>();
+   //  HashSet<String> variables = new HashSet<String>();
+    HashMap<String, VarType> variables = new HashMap<String, VarType>();
+    Stack<Value> stack = new Stack<Value>();
 
     @Override 
     public void exitProg(gramParser.ProgContext ctx) { 
@@ -14,33 +28,106 @@ public class LLVMActions extends gramBaseListener {
     @Override
     public void exitAssign(gramParser.AssignContext ctx) { 
        String ID = ctx.ID().getText();
-       String INT = ctx.INT().getText();
-       if( ! variables.contains(ID) ) {
-          variables.add(ID);
-          LLVMGenerator.declare(ID);          
-       } 
-       LLVMGenerator.assign(ID, INT);
+       Value  v  = stack.pop();
+       variables.put(ID, v.type);
+       if(v.type == VarType.INT){
+         LLVMGenerator.declare_i32(ID);
+         LLVMGenerator.assign_i32(ID, v.name);
+       }
+       if(v.type == VarType.REAL){
+         LLVMGenerator.declare_double(ID);
+         LLVMGenerator.assign_double(ID, v.name);
+       }
     }
 
     @Override
+    public void exitInt(gramParser.IntContext ctx){
+       stack.push(new Value(ctx.INT().getText(),VarType.INT));
+    }
+
+    @Override
+    public void exitReal(gramParser.RealContext ctx){
+       stack.push(new Value(ctx.REAL().getText(),VarType.REAL));
+    }
+   
+   @Override
+   public void exitAdd(gramParser.AddContext ctx){
+      Value v1 = stack.pop();
+      Value v2 = stack.pop();
+      
+      if(v1.type == v2.type){
+         if(v1.type == VarType.INT){
+            LLVMGenerator.add_i32(v1.name, v2.name);
+            stack.push(new Value("%" + (LLVMGenerator.reg - 1), VarType.INT));
+         }
+         if(v1.type == VarType.REAL){
+            LLVMGenerator.add_double(v1.name, v2.name); 
+             stack.push( new Value("%"+(LLVMGenerator.reg - 1), VarType.REAL) ); 
+         }
+         else{
+            error(ctx.getStart().getLine(), "add type mismatch");
+         }
+      }
+   }
+
+   @Override
+   public void exitMul(gramParser.MulContext ctx){
+      Value v1 = stack.pop();
+      Value v2 = stack.pop();
+
+      if(v1.type == v2.type){
+         if(v1.type == VarType.INT){
+            LLVMGenerator.mult_i32(v1.name, v2.name);
+            stack.push(new Value("%"+(LLVMGenerator.reg - 1), VarType.INT));
+         }
+         if(v1.type == VarType.REAL){
+            LLVMGenerator.mult_double(v1.name,v2.name);
+            stack.push(new Value("%"+(LLVMGenerator.reg - 1),  VarType.REAL));
+         }
+         else{
+            error(ctx.getStart().getLine(), "multi type mismatch");
+         }
+      }
+   }
+
+   @Override
+   public void exitToint(gramParser.TointContext ctx){
+      Value v = stack.pop();
+      LLVMGenerator.fptosi(v.name);
+      stack.push(new Value("%"+(LLVMGenerator.reg - 1), VarType.INT));
+   }
+
+   @Override
+   public void exitToreal(gramParser.TorealContext ctx){
+      Value v = stack.pop();
+      LLVMGenerator.sitofp(v.name);
+      stack.push(new Value("%" + (LLVMGenerator.reg - 1),VarType.REAL));
+   }
+
+    @Override
     public void exitPrint(gramParser.PrintContext ctx) {
-       String ID = ctx.ID().getText();
-       if( variables.contains(ID) ) {
-          LLVMGenerator.printf( ID );
-       } else {
-          System.err.println("Line "+ ctx.getStart().getLine()+", unknown variable: "+ID);
-       }
+      //  String ID = ctx.ID().getText();
+      //  if( variables.contains(ID) ) {
+      //     LLVMGenerator.printf( ID );
+      //  } else {
+      //     System.err.println("Line "+ ctx.getStart().getLine()+", unknown variable: "+ID);
+      //  }
     } 
 
     @Override
     public void exitRead(gramParser.ReadContext ctx) {
-       String ID = ctx.ID().getText();
-       if( ! variables.contains(ID) ) {
-          variables.add(ID);
-          LLVMGenerator.declare(ID);          
-       } 
-       LLVMGenerator.scanf(ID);
-    } 
+      //  String ID = ctx.ID().getText();
+      //  if( ! variables.contains(ID) ) {
+      //     variables.add(ID);
+      //     LLVMGenerator.declare(ID);          
+      //  } 
+      //  LLVMGenerator.scanf(ID);
+    }
+    
+    void error(int line, String msg){
+      System.err.println("Error, line "+line+", "+msg);
+      System.exit(1);
+  } 
 
 
 }
--- LLVMGenerator.java
+++ LLVMGenerator.java
@@ -1,3 +1,4 @@
+import org.stringtemplate.v4.ST;
 
 class LLVMGenerator{
    
@@ -5,10 +6,17 @@ class LLVMGenerator{
    static String main_text = "";
    static int reg = 1;
 
-   static void printf(String id){
+   static void printf_i32(String id){
       main_text += "%"+reg+" = load i32, i32* %"+id+"\n";
       reg++;
-      main_text += "%"+reg+" = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strp, i32 0, i32 0), i32 %"+(reg-1)+")\n";
+      main_text += "%"+reg+" = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strpi, i32 0, i32 0), i32 %"+(reg-1)+")\n";
+      reg++;
+   }
+
+   static void printf_double(String id){
+      main_text += "%"+reg+" = load double, double* %"+id+"\n";
+      reg++;
+      main_text += "%"+reg+" = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strpd, i32 0, i32 0), double %"+(reg-1)+")\n";
       reg++;
    }
 
@@ -17,12 +25,59 @@ class LLVMGenerator{
       reg++;      
    }
 
-   static void declare(String id){
+   static void declare_i32(String id){
       main_text += "%"+id+" = alloca i32\n";
    }
 
-   static void assign(String id, String value){
+   static void declare_double(String id){
+      main_text += "%"+id+" = alloca double\n";
+   }
+
+   static void assign_i32(String id, String value){
       main_text += "store i32 "+value+", i32* %"+id+"\n";
+   }
+   static void assign_double(String id, String value){
+      main_text += "store double "+value+", double* %"+id+"\n";
+   }
+
+   static void load_i32(String id){
+      main_text += "%"+reg+" = load i32, i32* %"+id+"\n";
+      reg++;
+   }
+
+   static void load_double(String id){
+      main_text += "%"+reg+" = load double, double* %"+id+"\n";
+      reg++;
+   }
+
+   static void add_i32(String val1, String val2){
+      main_text += "%"+reg+" = add i32 "+val1+", "+val2+"\n";
+      reg++;
+   }
+
+   static void add_double(String val1, String val2){
+      main_text += "%"+reg+" = fadd double "+val1+", "+val2+"\n";
+      reg++;
+   }
+
+   static void mult_i32(String val1, String val2){
+      main_text += "%"+reg+" = mul i32 "+val1+", "+val2+"\n";
+      reg++;
+   }
+
+   static void mult_double(String val1, String val2){
+      main_text += "%"+reg+" = fmul double "+val1+", "+val2+"\n";
+      reg++;
+   }
+
+   static void sitofp(String id){
+      main_text += "%"+reg+" = sitofp i32 "+id+" to double\n";
+      reg++;
+   }
+
+   static void fptosi(String id){
+      main_text += "%"+reg+" = fptosi double "+id+" to i32\n";
+      reg++;
    }
 
    static String generate(){
